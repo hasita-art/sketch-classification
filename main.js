b0e83467aeb4e99089caa027e4425cac95cf14e9
hasita-art <73806182+hasita-art@users.noreply.github.com>
2022-02-06 12:26:02 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -7,8 +7,71 @@ sketch = quick_draw_data_set[random_number];
 
 document.getElementById("sketch_to_be_drawn")="Sketch to be drawn : "+sketch;
 
-time_counter=0;
+timer_counter=0;
 timer_check="";
 drawn_sketch="";
 answer_holder="";
 score=0;
+
+function updateCanvas(){
+    background("white");
+    random_number = Math.floor((Math.random()*quick_draw_data_set.length)+1);
+    console.log(quick_draw_data_set[random_number]);
+    sketch = quick_draw_data_set[random_number];
+    document.getElementById('sketch_name').innerHTML = 'sketch to be drawn : ' + sketch;
+}
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+}
+
+function draw(){
+    strokeWeight(10);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+    check_sketch();
+    if(drawn_sketch==sketch){
+        answer_holder = "set";
+        score++;
+        document.getElementById("score").innerHTML = "Score : " + score;
+    }
+}
+
+function check_sketch(){;
+    timer_counter++
+    document.getElementById("timer").innerHTML = "Timer : " + timer_counter;
+    console.log(timer_counter);
+    if(timer_counter>500){
+        timer_counter = 0;
+        timer_check = "Completed";
+    }
+    if(timer_check=="Completed"||asnwer_holder=="set"){
+        timer_check="";
+        answer_holder="";
+        updateCanvas();
+    }
+}
+
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    }
+    console.log(results);
+
+    drawn_sketch = results[0].label;
+    document.getElementById("your_sketch").innerHTML="Your Sketch : "+drawn_sketch;
+    document.getElementById('confidence').innerHTML = 'Confidence : ' + Math.round(results[0].confidence * 100) + '%';
+}
